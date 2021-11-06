9f2e0de21652aac088d2ea9575325002d7840353
Irina  Moonin <irina@beaconcure.com>
2021-11-06 14:33:25 +0200

    remove submodule

---
+++ src/main/poller_frontend/src/ServiceList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class ServiceList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toService = this.toService.bind(this);
+  }
+
+  toService(s) {
+    return (
+      <tbody key={s.id}>
+        <tr>
+          <td>{s.name}</td>
+          <td>{s.url}</td>
+          <td>{s.created_at}</td>
+          <td>{s.status}</td>
+        </tr>
+      </tbody>)
+  }
+
+  render() {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>URL</th>
+            <th>Created At</th>
+            <th>Status</th>
+          </tr>
+        </tbody>
+        {this.props.services.map(this.toService)}
+      </table>
+    )
+  }
+}
+
+export default ServiceList;
+
---
+++ src/main/poller_frontend/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ServiceList from "./ServiceList"
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      services: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("/api/services/status")
+      .then(response => response.text())
+      .then(text => this.setState({services: JSON.parse(text)}));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Services</h1>
+        <ServiceList services={this.state.services} />
+      </div>
+    )
+  }
+
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
